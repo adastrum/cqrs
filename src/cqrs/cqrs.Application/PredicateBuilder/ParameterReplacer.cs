f497abdc815eb592c2ae6b587b62caec1f7155cd
d.trofimov <d.trofimov@cloudpayments.ru>
2018-08-28 10:56:15 +0300

    Add predicate builder

---
+++ src/cqrs/cqrs.Application/PredicateBuilder/ParameterReplacer.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace cqrs.Application.PredicateBuilder
+{
+    public class ParameterReplacer : ExpressionVisitor
+    {
+        private readonly ParameterExpression _parameter;
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            return base.VisitParameter(_parameter);
+        }
+
+        internal ParameterReplacer(ParameterExpression parameter)
+        {
+            _parameter = parameter;
+        }
+    }
+}
---
+++ src/cqrs/cqrs.Application/PredicateBuilder/PredicateBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace cqrs.Application.PredicateBuilder
+{
+    public static class PredicateBuilder
+    {
+        private enum BinaryOperators
+        {
+            And,
+            Or
+        }
+
+        public static Expression<Func<T, bool>> True<T>() { return x => true; }
+
+        public static Expression<Func<T, bool>> False<T>() { return x => false; }
+
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            return expr1 == null
+                ? expr2
+                : Build(expr1, expr2, BinaryOperators.Or);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            return expr1 == null
+                ? expr2
+                : Build(expr1, expr2, BinaryOperators.And);
+        }
+
+        private static Expression<Func<T, bool>> Build<T>(Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2, BinaryOperators binaryOperator)
+        {
+            var body = binaryOperator == BinaryOperators.And
+                ? Expression.AndAlso(expr1.Body, expr2.Body)
+                : Expression.OrElse(expr1.Body, expr2.Body);
+
+            var parameterExpression = Expression.Parameter(typeof(T));
+            body = (BinaryExpression)new ParameterReplacer(parameterExpression).Visit(body);
+            return Expression.Lambda<Func<T, bool>>(body, parameterExpression);
+        }
+    }
+}
